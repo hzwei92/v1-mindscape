87a1ae9a57a72cb26a24b901ca4163f5428de5d3
H Z Wei <hzwei92@gmail.com>
2022-09-03 22:01:40 -0700

    experiment with fontsizes

--- client/src/features/twig/LinkTwig.tsx
+++ client/src/features/twig/LinkTwig.tsx
@@ -226,7 +226,7 @@ function LinkTwig(props: LinkTwigProps) {
                   isTab={!!twig.tabId}
                   isGroup={!twig.tabId && !!twig.groupId}
                   isWindow={!twig.tabId && !twig.groupId && !!twig.windowId}
-                  fontSize={20}
+                  fontSize={twig.isOpen ? 60 : 20}
                 />
                 <TwigControls 
                   twig={twig}
